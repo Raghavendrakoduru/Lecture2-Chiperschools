419a64fd13318f0d03ede396bff47dcf9b5a7884
Raghavendra Koduru <119060193+Raghavendrakoduru@users.noreply.github.com>
2024-06-06 07:40:09 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+    //Learning with patience and coding is best.
+    /*I Wanted to learn how to comment.
+    we have to code daily for one hour/*
+}
